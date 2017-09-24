039cf75478f99c9c11054de37f6b751ff3ec982d
Kyle Brown <picokyle@gmail.com>
2017-09-24 11:12:44 -0400

    Initial commit

---
+++ client.go
@@ -0,0 +1,59 @@
+package gopherclient
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type ItemType byte
+
+const (
+	TEXT_FILE = ItemType('0')
+	DIRECTORY = ItemType('1')
+	INFO      = ItemType('i')
+)
+
+type Item struct {
+	Type        ItemType
+	Description string
+	Selector    string
+	Host        string
+	Port        int
+}
+
+func parseItem(line string) (*Item, error) {
+	item := Item{}
+	parts := strings.Split(line, "\t")
+
+	if len(parts[0]) == 0 {
+		return nil, errors.New(fmt.Sprintf("no item type: \"%s\"", line))
+	}
+
+	item.Type = ItemType(parts[0][0])
+	item.Description = parts[0][1:]
+
+	if len(parts) > 1 {
+		item.Selector = parts[1]
+	}
+
+	if len(parts) > 2 && parts[2] != "" {
+		item.Host = parts[2]
+	} else {
+		item.Host = "null.host"
+	}
+
+	if len(parts) > 3 && parts[3] != "" {
+		port, err := strconv.Atoi(parts[3])
+		if err != nil {
+			return nil, err
+		}
+		item.Port = port
+	} else {
+		item.Port = 70
+	}
+
+	return &item, nil
+}
+}
---
+++ client_test.go
@@ -0,0 +1,198 @@
+package gopherclient
+
+import (
+	"fmt"
+	"testing"
+)
+
+func itemTypeName(itemType ItemType) string {
+	itemTypes := map[ItemType]string{
+		TEXT_FILE: "TEXT_FILE",
+		DIRECTORY: "DIRECTORY",
+		INFO:      "INFO",
+	}
+	name, found := itemTypes[itemType]
+	if !found {
+		return "UNKNOWN"
+	} else {
+		return name
+	}
+}
+
+func checkItemType(actual ItemType, expected ItemType, t *testing.T) {
+	if actual != expected {
+		t.Errorf("Expected type %d (%s) to equal %d (%s).",
+			actual, itemTypeName(actual),
+			expected, itemTypeName(expected))
+	}
+}
+
+func checkExpected(actual interface{}, expected interface{}, t *testing.T) {
+	if actual != expected {
+		t.Fatalf("Expected \"%v\" to equal \"%v\".", actual, expected)
+	}
+}
+
+func checkEmpty(actual string, t *testing.T) {
+	if actual != "" {
+		t.Fatalf("Expected \"%s\" to be empty.", actual)
+	}
+}
+
+func unexpectedParseError(err error, t *testing.T) {
+	t.Fatalf("Unexpected error while parsing item: %s", err)
+}
+
+func TestParseEmptyStringReturnsError(t *testing.T) {
+	if _, err := parseItem(""); err == nil {
+		t.FailNow()
+	}
+}
+
+func TestParseTEXT_FILEItemType(t *testing.T) {
+	item, err := parseItem("0")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkItemType(item.Type, TEXT_FILE, t)
+	}
+}
+
+func TestParseDIRECTORYItemType(t *testing.T) {
+	item, err := parseItem("1")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkItemType(item.Type, DIRECTORY, t)
+	}
+}
+
+func TestParseINFOItemType(t *testing.T) {
+	item, err := parseItem("i")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkItemType(item.Type, INFO, t)
+	}
+}
+
+func TestParseDescriptionDefaultsToEmptyString(t *testing.T) {
+	item, err := parseItem("0")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkEmpty(item.Description, t)
+	}
+}
+
+func TestParseDescriptionAfterItemType(t *testing.T) {
+	description := "Some description text"
+	item, err := parseItem("0" + description)
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Description, description, t)
+	}
+}
+
+func TestParseDescriptionUpToTab(t *testing.T) {
+	description := "Some description text"
+	item, err := parseItem(fmt.Sprintf("0%s\tmore text", description))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Description, description, t)
+	}
+}
+
+func TestParseSelectorDefaultsToEmptyString(t *testing.T) {
+	item, err := parseItem("0")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkEmpty(item.Selector, t)
+	}
+}
+
+func TestParseSelectorAfterDescription(t *testing.T) {
+	selector := "a selector"
+	item, err := parseItem(fmt.Sprintf("0a description\t%s", selector))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Selector, selector, t)
+	}
+}
+
+func TestParseSelectorUpToTab(t *testing.T) {
+	selector := "a selector"
+	item, err := parseItem(fmt.Sprintf("0a description\t%s\tblah", selector))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Selector, selector, t)
+	}
+}
+
+func TestParseHostDefaultsToNullHost(t *testing.T) {
+	host := "null.host"
+	item, err := parseItem("0a description\ta selector\t")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Host, host, t)
+	}
+}
+
+func TestParseHostAfterSelector(t *testing.T) {
+	host := "a host"
+	item, err := parseItem(fmt.Sprintf("0a description\ta selector\t%s", host))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Host, host, t)
+	}
+}
+
+func TestParseHostUpToTab(t *testing.T) {
+	host := "a host"
+	item, err := parseItem(
+		fmt.Sprintf("0a description\ta selector\t%s\t1", host))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Host, host, t)
+	}
+}
+
+func TestParsePortDefaultsTo70(t *testing.T) {
+	port := 70
+	item, err := parseItem("0a description\ta selector\ta host\t")
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Port, port, t)
+	}
+}
+
+func TestParsePortAfterHost(t *testing.T) {
+	port := 50
+	item, err := parseItem(
+		fmt.Sprintf("0description\tselector\thost\t%d", port))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Port, port, t)
+	}
+}
+
+func TestParsePortUpToTab(t *testing.T) {
+	port := 50
+	item, err := parseItem(
+		fmt.Sprintf("0description\tselector\thost\t%d\tblah", port))
+	if err != nil {
+		unexpectedParseError(err, t)
+	} else {
+		checkExpected(item.Port, port, t)
+	}
+}
